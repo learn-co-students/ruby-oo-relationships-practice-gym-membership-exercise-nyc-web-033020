0d5b819b438b8e00a8439aa2ba4f63b8869c96a8
dkersten <dkersten1994@gmail.com>
2020-04-07 07:15:48 -0400

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,43 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    Gym.all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+
+  def lifters
+    memberships.map do |membership|
+      membership.lifter
+    end
+  end
+
+  def names
+    lifters.map do |lifter|
+      lifter.name
+    end
+  end
+
+  def combined_lift_total
+    arr_lift_total = []
+
+    lifters.each do |lifter|
+      arr_lift_total << lifter.lift_total
+    end
+    arr_lift_total.reduce(0) {|sum, n| sum + n}
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,23 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+lifter1 = Lifter.new("Chuck", 800)
+lifter2 = Lifter.new("Sarah", 1100)
+lifter3 = Lifter.new("Casey", 2000)
+lifter4 = Lifter.new("Morgan", 400)
+lifter5 = Lifter.new("Devon", 1900)
+
+gym1 = Gym.new("Gym 1")
+gym2 = Gym.new("Gym 2")
+gym3 = Gym.new("Gym 3")
+gym4 = Gym.new("Gym 4")
+gym5 = Gym.new("Gym 5")
+
+membership1 = Membership.new(20, gym5, lifter1)
+membership2 = Membership.new(40, gym2, lifter2)
+membership3 = Membership.new(15, gym2, lifter1)
+membership4 = Membership.new(25, gym2, lifter2)
+membership5 = Membership.new(30, gym5, lifter1)
 
 binding.pry
 
